1d99c156d6e8938990e9a1ce780756a68ebf6a50
James534 <james534@outlook.com>
2016-07-24 23:52:36 -0400

    buoytask as of day -1 of competition

--- src/controller/task/BuoyTask.cpp
+++ src/controller/task/BuoyTask.cpp
@@ -7,12 +7,39 @@ BuoyTask::BuoyTask(Model *camModel, TurnTask *tk, SpeedTask *st, DepthTask *dt){
     this->camModel = dynamic_cast<CameraModel*>(camModel);
     this->tk = tk;
     this->st = st;
-    this->dt = dt;
-
-    // Load properties file
-    PropertyReader* propReader;
+    this->dt = dt;// Load properties file
     propReader = new PropertyReader("settings/buoy_task_settings.txt");
     settings = propReader->load();
+    erosion_size = std::stoi(settings->getProperty("erosion_size"));
+    dilation_size = std::stoi(settings->getProperty("dilation_size"));
+    thresh = std::stoi(settings->getProperty("thresh"));
+    max_thresh = std::stoi(settings->getProperty("max_thresh"));
+    alignRadius = std::stoi(settings->getProperty("alignRadius"));
+    lowRedH = std::stoi(settings->getProperty("lowRedH"));
+    lowRedS = std::stoi(settings->getProperty("lowRedS"));
+    lowRedV = std::stoi(settings->getProperty("lowRedV"));
+    highRedH = std::stoi(settings->getProperty("highRedH"));
+    highRedS = std::stoi(settings->getProperty("highRedS"));
+    highRedV = std::stoi(settings->getProperty("highRedV"));
+    lowRedH2 = std::stoi(settings->getProperty("lowRedH2"));
+    lowRedS2 = std::stoi(settings->getProperty("lowRedS2"));
+    lowRedV2 = std::stoi(settings->getProperty("lowRedV2"));
+    highRedH2 = std::stoi(settings->getProperty("highRedH2"));
+    highRedS2 = std::stoi(settings->getProperty("highRedS2"));
+    highRedV2 = std::stoi(settings->getProperty("highRedV2"));
+    lowGreenH = std::stoi(settings->getProperty("lowGreenH"));
+    lowGreenS = std::stoi(settings->getProperty("lowGreenS"));
+    lowGreenV = std::stoi(settings->getProperty("lowGreenV"));
+    highGreenH = std::stoi(settings->getProperty("highGreenH"));
+    highGreenS = std::stoi(settings->getProperty("highGreenS"));
+    highGreenV = std::stoi(settings->getProperty("highGreenV"));
+    lowGreenH2 = std::stoi(settings->getProperty("lowGreenH2"));
+    lowGreenS2 = std::stoi(settings->getProperty("lowGreenS2"));
+    lowGreenV2 = std::stoi(settings->getProperty("lowGreenV2"));
+    highGreenH2 = std::stoi(settings->getProperty("highGreenH2"));
+    highGreenS2 = std::stoi(settings->getProperty("highGreenS2"));
+    highGreenV2 = std::stoi(settings->getProperty("highGreenV2"));
+
 }
 
 BuoyTask ::~BuoyTask(){
@@ -34,27 +61,31 @@ void BuoyTask::execute() {
     float * centerY = new float();//NULL; ///Pointer for Y Coor of Circle Center
     int delta = 0; ///Change in Movement Needed
     int deltaNext = 0; ///Moving to next buoy
+    int deltaTurn = 0; ///Turning to next buoy
     int deltaGreen = 0; ///Moving to the green buoy
     int deltaYellow = 0; ///Moving to the yellow buoy
-    int thresholdRadius = std::stoi(settings->getProperty("thresholdRadius"));
-    int alignRadius = std::stoi(settings->getProperty("alignRadius"));
 
     // Set up data the sub is receiving
     std::string s = "raw";
     ImgData *data;
     int imgWidth, imgHeight;
 
-    st->setTargetSpeed(0);
-    //std::stoi(settings->getProperty("targetSpeed")));
-    st->execute();
-
     cout << "getting state " << camModel->getState() << endl;
 
+    st->setTargetSpeed(30);
+    st->execute();
+    sleep(1);
+
+    st->setTargetSpeed(0);
+    st->execute();
+    sleep(1);
     // Detect buoys while the sub is not done the task
     while (!done){
         data = dynamic_cast<ImgData*>(dynamic_cast<CameraState*>(camModel->getState())->getDeepState(s));
         imgWidth = data->getImg().size().width;
         imgHeight = data->getImg().size().height;
+        int closeRadius = round(imgWidth/3.5);
+        int thresholdRadius = round(imgWidth/1.5);
 
         // Find the buoy parameters
         logger->info("detecting buoys");
@@ -62,83 +93,163 @@ void BuoyTask::execute() {
 
         // Do nothing if no buoys have been found
         if (*radius == 0) {
-            logger->info("none found");
+            if (stage == "GREEN") {
+                tk->setYawDelta(25);
+                tk->execute();
+                sleep(3);
+            }
+            else {
+                logger->info("none found");
+                tk->setYawDelta(45);
+                tk->execute();
+                sleep(5);
+                data = dynamic_cast<ImgData*>(dynamic_cast<CameraState*>(camModel->getState())->getDeepState(s));
+                detectBuoy(data, stage, centerX, centerY, radius);
+                if (*radius == 0) {
+                    logger->info("none found");
+                    tk->setYawDelta(-90);
+                    tk->execute();
+                    sleep(5);
+                    data = dynamic_cast<ImgData*>(dynamic_cast<CameraState*>(camModel->getState())->getDeepState(s));
+                    detectBuoy(data, stage, centerX, centerY, radius);
+                    if (*radius == 0) {
+                        tk->setYawDelta(45);
+                        tk->execute();
+                        sleep(5);
+                        st->setTargetSpeed(20);
+                        st->execute();
+                        sleep(5);
+                        st->setTargetSpeed(0);
+                        st->execute();
+                        sleep(1);
+                    }
+                    else {
+                        st->setTargetSpeed(20);
+                        sleep(1);
+                        st->setTargetSpeed(0);
+                        st->execute();
+                        sleep(1);
+                    }
+                }
+                else {
+                    st->setTargetSpeed(20);
+                    st->execute();
+                    sleep(1);
+                    st->setTargetSpeed(0);
+                    st->execute();
+                    sleep(1);
+                }
+            }
         }
         // A buoy has been detected in the distance
         else if ((0 < *radius) && (*radius < thresholdRadius) && (!transition))
         {
-                logger->info("found");
-                logger->info(std::to_string(imgWidth));
-                logger->info(std::to_string(*centerX) + "centerx");
-                if (!(((imgWidth/2 - alignRadius) < *centerX ) && (*centerX < (imgWidth/2 + alignRadius)))){
+            logger->info("found");
+            logger->info(std::to_string(imgWidth));
+            logger->info(std::to_string(*centerX) + "");
 
+            if (*radius < thresholdRadius && !transition) {
+//                st->setTargetSpeed(0);
+//                st->execute();
+                // Move towards the buoy until contact is made
+                if (!(std::abs(*centerX - imgWidth/2) < imgWidth/25)){
+                    // If the buoy is not within the middle 10% of the camera image
+                    if (*centerX > imgWidth/2) {
+                        delta = 8;
+                    } else if (*centerX < imgWidth/2) {
+                        delta = -8;
+                    }
                     // Align the sub to the x-coordinate of the detected buoy
                     logger->info("aligning");
-                    delta = atan((*centerX - imgWidth/2)/imgHeight/2)*(2*M_PI/360);
                     logger->info("Rotating sub by " + std::to_string(delta) + " degrees.");
                     tk->setYawDelta(delta);
                     tk->execute();
-                    usleep(1000000);
+                    sleep(5);
+                    continue;
                 }
-                if (!(imgHeight/2 - alignRadius < *centerY and *centerY < imgHeight/2 + alignRadius)){
 
+                if (!(std::abs(*centerY - imgHeight/2) < imgHeight/25)){
+                    // If the buoy is not within the middle 10% of the camera image
+                    // usleep(1000000);
+                    if (*centerY > imgHeight/2) {
+                        delta = 5;
+                        if (*radius > closeRadius) {
+                            delta = 2;
+                        }
+                    } else if (*centerY < imgHeight/2) {
+                        delta = -5;
+                    }
                     // Align the sub to the y-coordinate of the detected buoy
-                    delta = (imgHeight/2 - *centerY)/20;
-                    logger->debug("Changing height by " + std::to_string(delta));
-                    tk->setYawDelta(delta);
-                    tk->execute();
-                    usleep(5000000);
+                    logger->info("aligning");
+                    logger->info("Changing depth by " + std::to_string(delta) + " units.");
+                    dt->setDepthDelta(delta);
+                    dt->execute();
+                    sleep(10);
+                    continue;
                 }
+                st->setTargetSpeed(20);
+                st->execute();
+                sleep(2);
+                st->setTargetSpeed(0);
+                st->execute();
+                sleep(2);
+                data = dynamic_cast<ImgData*>(dynamic_cast<CameraState*>(camModel->getState())->getDeepState(s));
+                detectBuoy(data, stage, centerX, centerY, radius);
+            }
+
         // Transition to the next buoy if the sub is sufficiently close to the current buoy
         } else if (*radius >= thresholdRadius && !transition){
-            logger->info("green");
-                if (stage == "RED"){
-                    stage = "GREEN";
-                    transition = true;
-                    usleep(10000000);
-                } else if (stage == "GREEN"){
-                    stage = "YELLOW";
-                    transition = true;
-                    usleep(10000000);
-
-                // The sub has hit the last buoy
-                } else{
-                    done = true;
-                }
+            st->setTargetSpeed(10);
+            st->execute();
+            sleep(2);
+            st->setTargetSpeed(-5);
+            st->execute();
+            sleep(2);
+            st->setTargetSpeed(0);
+            st->execute();
+            logger->info("The" + stage + "buoy has been hit");
+            if (stage == "RED"){
+                stage = "GREEN";
+                transition = true;
+                sleep(10);
+            }
+            // The sub has hit the last buoy
+            else{
+                logger->info("The buoy task has been completed");
+                done = true;
+            }
+            // Move away from the buoy
+            // st->setTargetSpeed(-50);
+            // st->execute();
         }
 
         // Move on to next buoy
         if (transition) {
-            logger->info("hm");
-             delta = deltaNext;
-             logger->debug("Changing speed by " + std::to_string(delta));
-             st->setTargetSpeed(0);//delta
-             st->execute();
-             usleep(100000000);
 
-             // Orient the sub to the next buoy
-             if (stage == "GREEN") {
-                 delta = deltaGreen;
-                 logger->debug("Orienting to GREEN");
-                 tk->setYawDelta(delta);
-                 tk->execute();
-                 usleep(10000000);
-             } else if (stage == "YELLOW"){
-                 delta = deltaYellow;
-                 logger->debug("Orienting to YELLOW");
-                 tk->setYawDelta(delta);
-                 tk->execute();
-                 usleep(10000000);
-             }
+            delta = deltaNext;
+            logger->debug("Changing speed by " + std::to_string(delta));
+            st->setTargetSpeed(delta);//delta
+            st->execute();
+            usleep(2000000);
+            logger->debug("Rotating sub by " + std::to_string(deltaTurn) + "to look for next buoy");
+            tk->setYawDelta(deltaTurn);
+            tk->execute();
+            sleep(5);
+
+            // Orient the sub to the next buoy
+            if (stage == "GREEN") {
+                delta = deltaGreen;
+                logger->debug("Orienting to GREEN");
+                tk->setYawDelta(delta);
+                tk->execute();
+                sleep(5);
+            }
         }
     }
 }
 
 void BuoyTask::thresh_callback(int, Mat all_colour_image, float *centerX, float *centerY, float *radius )
 {
-    logger->info("thresh_callback was called");
-    int thresh = std::stoi(settings->getProperty("thresh"));
-    int max_thresh = std::stoi(settings->getProperty("max_thresh"));
     RNG rng(12345);
     Mat canny_output;
     vector<vector<Point> > contours;
@@ -155,10 +266,8 @@ void BuoyTask::thresh_callback(int, Mat all_colour_image, float *centerX, float 
     // Get the mass centers:
     vector<Point2f> mc( contours.size() );
     for( int i = 0; i < mu.size(); i++ )
-    { mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
-    logger->info(std::to_string(mu[i].m10) + "mufirst");
-    logger->info(std::to_string(mu[i].m00) + "mu");
-    logger->info(std::to_string(mu[i].m01/mu[i].m00) + "third");
+    {
+        mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
     }
 
     // Draw contours
@@ -175,102 +284,79 @@ void BuoyTask::thresh_callback(int, Mat all_colour_image, float *centerX, float 
     // printf("\t Info: Area and Contour Length \n");
     for( int i = 0; i< contours.size(); i++ )
     {
-        // printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
         Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
         drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
         circle( drawing, mc[i], 4, color, -1, 8, 0 );
     }
+
     imshow("drawing", drawing);
 
     // Find largest mass
     float maxmass = 0;
+    float rad = 0;
     Point2f masscenter(0, 0);
-    for ( int i = 0; i < mc.size(); i++)
+    for ( int i = 0; i < contours.size(); i++)
     {
-        float cur = pow(contourArea(contours[i])/M_PI,1/2);
-            if (maxmass < cur && (abs(cur) == abs(cur)) & (cur < 5000))
+        float cur = cv::contourArea(contours[i]);
+            if ((maxmass < cur))
             {
-            masscenter = mc[i];
+            masscenter = cv::Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );;
             maxmass = cur;
+            rad = pow(contourArea(contours[i])/M_PI,1/2);
             }
     }
     logger->info(std::to_string(maxmass) + " radius");
     logger->info("mass center: " + to_string(masscenter.x) + " " + to_string(masscenter.y));
     *centerX = masscenter.x;
     *centerY = masscenter.y;
-    *radius = maxmass;
+    *radius = rad;
     circle( drawing, masscenter, 200, Scalar(0,0,255), 5);
 }
 
 void BuoyTask::detectBuoy(ImgData* data, std::string stage, float *centerX, float *centerY, float *radius)
 {
+    logger->info("DETECTING BUOY");
     Mat src, src_HSV;
-    Mat lower_hue_range_image, upper_hue_range_image;
     src = data->getImg();
 
     // Convert image to HSV and blur it
     cvtColor( src, src_HSV, CV_BGR2HSV );
     blur( src_HSV, src_HSV, Size(3,3) );
 
-    Mat colour_image;
+    Mat colour_image1, colour_image2, colour_image;
 
     imshow("regular", data->getImg());
 
     if (stage == "RED")
     {
-        logger->info("Stage is RED");
         // Filter the image for red
-        inRange(src_HSV, Scalar(std::stoi(settings->getProperty("lowRedH")),
-                std::stoi(settings->getProperty("lowRedS")),
-                std::stoi(settings->getProperty("lowRedV"))),
-                Scalar(std::stoi(settings->getProperty("highRedH")),
-                        std::stoi(settings->getProperty("highRedS")),
-                        std::stoi(settings->getProperty("highRedV"))), lower_hue_range_image);
-        inRange(src_HSV, Scalar(std::stoi(settings->getProperty("upperLowRedH")),
-                std::stoi(settings->getProperty("upperLowRedS")),
-                std::stoi(settings->getProperty("upperLowRedV"))),
-                Scalar(std::stoi(settings->getProperty("upperHighRedH")),
-                        std::stoi(settings->getProperty("upperHighRedS")),
-                        std::stoi(settings->getProperty("upperHighRedV"))), upper_hue_range_image);
-        addWeighted(lower_hue_range_image, 1.0, upper_hue_range_image, 1.0, 0.0, colour_image);
+        // Lower red colour range
+        inRange(src_HSV, Scalar(lowRedH, lowRedS, lowRedV),
+                Scalar(highRedH, highRedS, highRedV), colour_image1);
+
+        inRange(src_HSV, Scalar(lowRedH2, lowRedS2, lowRedV2),
+                Scalar(highRedH2, highRedS2, highRedV2), colour_image2);
+        // Upper red colour range
+        addWeighted(colour_image1, 1.0, colour_image2, 1.0, 0.0, colour_image);
+
     } else if (stage == "GREEN")
     {
-        logger->info("Stage is GREEN");
         // Filter for green
-        inRange(src_HSV, Scalar(std::stoi(settings->getProperty("lowGreenH")),
-                        std::stoi(settings->getProperty("lowGreenS")),
-                        std::stoi(settings->getProperty("lowGreenV"))),
-                        Scalar(std::stoi(settings->getProperty("highGreenH")),
-                                std::stoi(settings->getProperty("highGreenS")),
-                                std::stoi(settings->getProperty("highGreenV"))), lower_hue_range_image);
-                inRange(src_HSV, Scalar(std::stoi(settings->getProperty("upperLowGreenH")),
-                       std::stoi(settings->getProperty("upperLowGreenS")),
-                        std::stoi(settings->getProperty("upperLowGreenV"))),
-                        Scalar(std::stoi(settings->getProperty("upperHighGreenH")),
-                                std::stoi(settings->getProperty("upperHighGreenS")),
-                                std::stoi(settings->getProperty("upperHighGreenV"))), upper_hue_range_image);
-        addWeighted(lower_hue_range_image, 1.0, upper_hue_range_image, 1.0, 0.0, colour_image);
-    } else
-    {
-        logger->info("Stage is YELLOW");
-        // Filter for yellow
-        inRange(src_HSV, Scalar(std::stoi(settings->getProperty("lowYellowH")),
-                std::stoi(settings->getProperty("lowYellowS")),
-                std::stoi(settings->getProperty("lowYellowV"))),
-                Scalar(std::stoi(settings->getProperty("highYellowH")),
-                        std::stoi(settings->getProperty("highYellowS")),
-                        std::stoi(settings->getProperty("highYellowV"))), colour_image);
-    }
+        // Lower green colour range
+        inRange(src_HSV, Scalar(lowGreenH, lowGreenS, lowGreenV),
+                Scalar(highGreenH, highGreenS, highGreenV), colour_image1);
 
-    logger->info("image has been filtered");
+        inRange(src_HSV, Scalar(lowGreenH2, lowGreenS2, lowGreenV2),
+                Scalar(highGreenH2, highGreenS2, highGreenV2), colour_image2);
+
+        addWeighted(colour_image1, 1.0, colour_image2, 1.0, 0.0, colour_image);
+    }
 
     // Erosion and Dilation
     int dilation_type;
     int erosion_type;
     int erosion_elem = 0;
-    int erosion_size = std::stoi(settings->getProperty("erosion_size"));
     int dilation_elem = 0;
-    int dilation_size = std::stoi(settings->getProperty("dilation_size"));
 
     if( erosion_elem == 0 ){ erosion_type = MORPH_RECT; }
     else if( erosion_elem == 1 ){ erosion_type = MORPH_CROSS; }
@@ -297,6 +383,7 @@ void BuoyTask::detectBuoy(ImgData* data, std::string stage, float *centerX, floa
     imshow("filtered", colour_image);
 
     waitKey(1);
+    printf("center: %f, %f, %f\n", centerX, centerY, radius);
     // Find the largest mass
     thresh_callback( 0, colour_image, centerX, centerY, radius);
 }
--- src/controller/task/BuoyTask.h
+++ src/controller/task/BuoyTask.h
@@ -40,6 +40,39 @@ private:
     DepthTask* dt;
     void thresh_callback(int, Mat, float*, float*, float*);
     void detectBuoy(ImgData*, std::string, float*, float*, float*);
+
+    // Load properties file
+    PropertyReader* propReader;
+    int erosion_size;
+    int dilation_size;
+    int thresh;
+    int max_thresh;
+    int alignRadius;
+    int lowRedH;
+    int lowRedS;
+    int lowRedV;
+    int highRedH;
+    int highRedS;
+    int highRedV;
+    int lowRedH2;
+    int lowRedS2;
+    int lowRedV2;
+    int highRedH2;
+    int highRedS2;
+    int highRedV2;
+    int lowGreenH;
+    int lowGreenS;
+    int lowGreenV;
+    int highGreenH;
+    int highGreenS;
+    int highGreenV;
+    int lowGreenH2;
+    int lowGreenS2;
+    int lowGreenV2;
+    int highGreenH2;
+    int highGreenS2;
+    int highGreenV2;
+
 };
 
 #endif // BUOYTASK_H
