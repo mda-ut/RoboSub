f80d5ada34eb2771836cc5251316a7ae0afac745
James534 <james534@outlook.com>
2016-07-27 18:28:18 -0400

    Changed main file

--- src/Main.cpp
+++ src/Main.cpp
@@ -16,6 +16,8 @@
 #include "StateTester.h"
 #include "VideoTesting.h"
 
+bool videoTest = false;
+
 int main(int argc, char** argv) {
     Timer* logTimer = new Timer();
     QApplication app(argc, argv);
@@ -75,9 +77,21 @@ int main(int argc, char** argv) {
 
     std::string mode = settings->getProperty("MODE");
 
-    if (mode == "HSVTEST") {
-        //VideoTesting vt(0); //camera id or file name
-        //vt.run(1); //0 = video; 1 = webcam; 2 = image
+    if (mode == "TEST") {
+        int vtMode = std::stoi(settings->getProperty("vtMode"));
+        printf("mode [%d]\n", vtMode);
+        VideoTesting *vt;
+        if (vtMode != 1){
+            std::string filename = settings->getProperty("vtFilename");
+            vt = new VideoTesting(filename); //camera id or file name
+        }else{
+            int vtCam = std::stoi(settings->getProperty("vtFilename"));
+            printf("opening [%d]\n", vtCam);
+            vt = new VideoTesting(vtCam);
+        }
+        vt->run(vtMode);                   //0 = video; 1 = webcam; 2 = image
+        delete(vt);
+        return 0;
     } else {
         init_signal_handler();
 
@@ -88,8 +102,6 @@ int main(int argc, char** argv) {
         mainStage->initialize();
 
         delete propReader;
+        return app.exec();
     }
-    return app.exec();
-//    VideoTesting vt(0); //camera id or file name
-//    vt.run(1); //0 = video; 1 = webcam; 2 = imag
 }
